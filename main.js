41584f6165c052f92bce42bc4f37edd8f3c5dfeb
Faristol <joseptortosa33@gmail.com>
2024-01-04 18:17:01 +0100

    moviment funcional

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const board = 500;
 const cell = 10;
 let canvas = null;
+let id = 0;
 
 //350x350
-const snake = [
+let snake = [
   { x: 2, y: 0 },
   { x: 1, y: 0 },
   { x: 0, y: 0 },
 ];
-const food = { x: 10, y: 40 };
+let food = { x: 10, y: 40 };
 //const snakeSpeed = 1;
 //default -> right
-const currentDirection = { x: 1, y: 0 };
+let currentDirection = { x: 1, y: 0 };
 
 const inici = () => {
   canvas = document.querySelector("#cv");
@@ -27,31 +28,35 @@ const movementControl = (e) => {
     case "ArrowUp":
       //x cte y--
       console.log("ArrowUp");
+      currentDirection = { x: 0, y: -1 };
       break;
     case "ArrowDown":
       //x cte y++
+      currentDirection = { x: 0, y: 1 };
       console.log("ArrowDown");
       break;
     case "ArrowLeft":
       //x-- y cte
+      currentDirection = { x: -1, y: 0 };
       console.log("ArrowLeft");
       break;
     case "ArrowRight":
       //x++ y cte
+      currentDirection = { x: 1, y: 0 };
       console.log("ArrowRight");
       break;
   }
 };
 const game = () => {
+  window.clearInterval(id);
   draw();
   update();
-  window.requestAnimationFrame(game);
+  id=window.setInterval(game,200);
 };
-const update = () => {};
-console.log(canvas)
 const draw = () => {
   if(canvas.getContext){
     ctx = canvas.getContext("2d");
+    ctx.clearRect(0,0,canvas.width,canvas.width);
     snake.forEach((segment)=>{
       ctx.fillStyle = "green";
       ctx.fillRect(segment.x*cell,segment.y*cell,cell,cell);
@@ -60,9 +65,25 @@ const draw = () => {
       ctx.fillStyle = "red";
       ctx.fillRect(food.x*cell,food.y*cell,cell,cell);
     }
-    
   }
-  
+};
+const update = () => {
+  //sha de fer una copia profunda per a que no mantinga la referencia amb [...snake] no serviria,
+  //done fe
+  let copySnake = JSON.parse(JSON.stringify(snake));
+
+   //el cap marca la direcció, els altres segments agafen la x i la y del segment anterior
+  for (let i = 0; i < snake.length; i++) {
+    if (i === 0) {
+      snake[i].x += currentDirection.x;
+      snake[i].y += currentDirection.y;
+    } else {
+      snake[i].x = copySnake[i - 1].x;
+      snake[i].y = copySnake[i - 1].y;
+    }
+  }
+
+  console.log(snake);
 };
 
 
